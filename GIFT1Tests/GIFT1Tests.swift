fbdd35ae36dbc014e3372b892a27bd5e9728b47d
guild1213 <99534743+guild1213@users.noreply.github.com>
2022-02-12 22:41:24 +0900

    Add files via upload

---
+++ GIFT1Tests/GIFT1Tests.swift
@@ -0,0 +1,33 @@
+//
+//  GIFT1Tests.swift
+//  GIFT1Tests
+//
+//  Created by 朝倉健登 on 2021/09/27.
+//
+
+import XCTest
+@testable import GIFT1
+
+class GIFT1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
